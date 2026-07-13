4faad9576e0a1c3a3ad0df631dca190b9d2fe040
aariz0012 <khanaariz0012@gmail.com>
2026-07-13 15:50:35 +0530

    Remove conditional base path - rely on GitHub Actions build flag

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Portfolio/' : '/',
   build: {
     assetsDir: 'assets',
     copyPublicDir: true
